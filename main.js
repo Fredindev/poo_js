6e6421074bd8b24f18bb94686a49111a14c6488c
Fredindev <multverso2020@gmail.com>
2024-08-21 15:38:54 -0300

    corrigindo o código

--- main.js
+++ main.js
@@ -1,45 +1,65 @@
 class DispositivoEletronico {
-    aparelho(marca, modelo) {
-      this.marca = marca;
-      this.modelo = modelo;
-      if (new.target === DispositivoEletronico) {
-        throw new Error('Esta classe não pode ser instanciada diretamente');
-      }
+  constructor(marca, modelo) {
+    this.marca = marca;
+    this.modelo = modelo;
+    if (new.target === DispositivoEletronico) {
+      throw new Error('Esta classe não pode ser instanciada diretamente');
     }
   }
-  
-  class Smartphone extends DispositivoEletronico {
-    aparelho(marca, modelo, sistemaOperacional) {
-      desing(marca, modelo);
-      this.sistemaOperacional = sistemaOperacional;
-    }
-  
-    ligar() {
-      console.log('Ligando o smartphone...');
-    }
-  
-    desligar() {
-      console.log('Desligando o smartphone...');
-    }
+}
+
+class Smartphone extends DispositivoEletronico {
+  constructor(marca, modelo, sistemaOperacional) {
+    super(marca, modelo); 
+    this.sistemaOperacional = sistemaOperacional;
+  }
+
+  ligar() {
+    console.log('Ligando o smartphone...');
+  }
+
+  desligar() {
+    console.log('Desligando o smartphone...');
+  }
+}
+
+class Computador extends DispositivoEletronico {
+  constructor(marca, modelo, processador) {
+    super(marca, modelo); 
+    this.processador = processador;
+  }
+
+  ligar() {
+    console.log('Ligando o computador...');
+  }
+
+  desligar() {
+    console.log('Desligando o computador...');
+  }
+}
+
+
+class Tablet extends DispositivoEletronico {
+  constructor(marca, modelo, sistemaOperacional, penStylus) {
+    super(marca, modelo);
+    this.sistemaOperacional = sistemaOperacional;
+    this.penStylus = penStylus;
   }
   
-  class Computador extends DispositivoEletronico {
-    aparelho(marca, modelo, processador) {
-      desing(marca, modelo);
-      this.processador = processador;
-    }
-  
-    ligar() {
-      console.log('Ligando o computador...');
-    }
-  
-    desligar() {
-      console.log('Desligando o computador...');
-    }
+  ligar() {
+    console.log('Ligando o tablet...');
   }
   
-  const smartphone1 = new Smartphone('Apple', 'iPhone 16', 'iOS 18');
-  const computador1 = new Computador('Samsung', 'GalaxyS25', 'Android 15');
-  
-  smartphone1.ligar();
-  computador1.desligar();
+  desligar() {
+    console.log('Desligando o tablet...');
+  }
+}
+
+const smartphone1 = new Smartphone('Apple', 'iPhone 16', 'iOS 18');
+const computador1 = new Computador('Dell', 'Alienware', 'Intel Raptor lake 14');
+const tablet1 = new Tablet('Samsung', 'Galaxy Tab S10', 'Android 18', 'Stylus');
+
+
+smartphone1.ligar();
+computador1.desligar();
+tablet1.ligar();
